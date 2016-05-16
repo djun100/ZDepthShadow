33575868de972d88c5c57a87dedd141f750426f2
djun100 <djun100@qq.com>
2016-05-16 08:04:58 +0800

    init

---
+++ app/src/main/java/com/cy/testapp/MainActivity2.java
@@ -0,0 +1,67 @@
+package com.cy.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends Activity {
+
+    /**@see gen */
+    /**@see gen.Tag*/
+    /**{@link gen.Tag}*/
+    /**{@link gen.TagStatic}*/
+    /**{@link #ALLOCATED_REQUEST_INDICIES_TAG}*/
+    /**{@link gen#tag_defind_start}*/
+    /**{@link gen#tag_define_start()}*/
+    /**@see gen#tag_define_start() */
+    /**{@linkplain gen}*/
+    /**{@literal }*/
+    /**{@link gen.Define_start}*/
+    /**{@link gen.Define_start}*/
+    /**@see gen.Define_start*/
+    //@see com.cy.testapp.gen
+    //{@link com.cy.testapp.gen#}
+    //genDefine_start
+    private TextView mTvTitle;
+    /**{@code mTvTitle = (TextView)findViewById(R.id.mTvTitle);<br/>
+    mBtnTitle = (Button)findViewById(R.id.mBtnTitle);}*/
+    private Button mBtnTitle;
+    /**@see gen
+    */
+    private TextView mTvHW;
+    /**@see gen.Tag*/
+    /***/
+    private Button mBtnHW;
+    private View.OnClickListener mOnClickListener;
+    //genDefine_end
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.writeE("dsf");
+        //genFindClick_start
+        setContentView(R.layout.activity_main);
+        mTvTitle = (TextView)findViewById(R.id.mTvTitle);
+        mBtnTitle = (Button)findViewById(R.id.mBtnTitle);
+        mTvHW = (TextView)findViewById(R.id.mTvHW);
+        mBtnHW = (Button)findViewById(R.id.mBtnHW);
+
+        mOnClickListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.mBtnTitle:
+                    break;
+                    case R.id.mBtnHW:
+                    break;
+                }
+            }
+        };
+        mBtnTitle.setOnClickListener(mOnClickListener);
+        mBtnHW.setOnClickListener(mOnClickListener);
+        //genFindClick_end
+    }
+
+
+}
---
+++ app/src/main/java/com/cy/testapp/gen.java
@@ -0,0 +1,23 @@
+package com.cy.testapp;
+
+/**
+ * Created by cy on 2016/3/26.
+ */
+public class gen {
+    public static final String tag_defind_start="tag_defind_start";
+    public static void tag_define_start(){
+
+    }
+    public class Tag{
+        public String tag_defind_start="tag_defind_start";
+        public void tag_define_start(){
+
+        }
+    }
+    public static class TagStatic{
+
+    }
+    class Define_start{
+
+    }
+}
